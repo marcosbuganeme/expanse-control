58caa4d7c7fcb7f506d4c1a5851a4f1cc915eae9
Marcos Buganeme <molavosbdeveloper@gmail.com>
2024-04-19 12:27:11 -0300

    chore: create struct of project

---
+++ src/main/java/br/com/expanse/control/ExpanseControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.expanse.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpanseControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpanseControlApplication.class, args);
+	}
+
+}
